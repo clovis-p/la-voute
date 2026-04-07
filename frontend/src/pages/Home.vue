6d06eae094d15a43787ccc3c916187e0857d44ab
clovis-p <clovis-p@proton.me>
2026-04-07 15:54:58 -0400

    [1] Ajouter frontend

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ titleMessage }}</h1>
+    <h2>{{ subtitleMessage }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const titleMessage = ref('');
+const subtitleMessage = ref('');
+
+onMounted(async () => {
+  const response = await axios.get('/api/home');
+  titleMessage.value = response.data.titleMessage;
+  subtitleMessage.value = response.data.subtitleMessage;
+})
+</script>
---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/main.js',
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+})
