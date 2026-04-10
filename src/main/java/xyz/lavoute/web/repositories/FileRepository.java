d347c8f659545361c8741cb04f6681caa2c4f057
MiaSwiishy <miasylveon002@gmail.com>
2026-04-10 08:12:37 -0400

    Revert "Merge pull request #31 from clovis-p/13-faire-le-modele-des-files-backend"
    This reverts commit 969aea2961bda796b459693b04d44c2148c2f2e2, reversing
    changes made to d78cbdb30f81de0a756f6c0db71769179b884bd3.

--- src/main/java/xyz/lavoute/web/repositories/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package xyz.lavoute.web.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import xyz.lavoute.web.models.File;
-
-public interface FileRepository extends CrudRepository<File, Integer> {
-}
