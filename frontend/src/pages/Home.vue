cc3a7136663a8bb17e8a97708744f5e454919069
clovis-p <clovis-p@proton.me>
2026-04-14 20:24:00 -0400

    [18] Prototype statique de liste de fichiers

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div class="flex flex-col h-screen">
+    <AppBar/>
+    <FileList/>
+  </div>
 </template>
+
+<script setup>
+import AppBar from "@/components/Dashboard/AppBar.vue";
+import FileList from "@/components/Dashboard/FileList.vue";
+</script>
