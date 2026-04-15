e0e181d02b3d6f764885e0b9b2e432e46528a594
WildSource <ilovetrap123@hotmail.com>
2026-04-15 19:17:29 -0400

    Add auth/login route to permitted paths

--- src/main/java/xyz/lavoute/web/security/SecurityConfig.java
+++ src/main/java/xyz/lavoute/web/security/SecurityConfig.java
@@ -11,9 +11,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        // Activer la protection csrf en prod
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register").permitAll() //Ajouter le /login pour autorisé le endpoint lorsque l'utilisateur n'est pas connecté
+                        .requestMatchers(
+                                "/register",
+                                "/auth/login"
+                        ).permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
