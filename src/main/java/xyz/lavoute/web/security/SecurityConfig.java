027d8e6c8a92af06a0f683a43e26067aa4aab248
WildSource <ilovetrap123@hotmail.com>
2026-04-17 15:37:34 -0400

    Basic auth with session token

--- src/main/java/xyz/lavoute/web/security/SecurityConfig.java
+++ src/main/java/xyz/lavoute/web/security/SecurityConfig.java
@@ -1,24 +1,42 @@
 package xyz.lavoute.web.security;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         // Activer la protection csrf en prod
-        http.csrf(csrf -> csrf.disable())
+        http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/register",
-                                "/auth/login"
-                        ).permitAll()
-                        .anyRequest().authenticated());
+                                "/login"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(login -> login
+                        .successHandler((req, res, auth) -> res.setStatus(200))
+                        .failureHandler((req, res, auth) -> res.setStatus(401))
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler((req, res, auth) -> res.setStatus(200))
+                );
         return http.build();
     }
 }
