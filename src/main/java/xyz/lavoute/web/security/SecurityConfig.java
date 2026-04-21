fd99dc5ccdd03f8e921024ab6bd926952d8f8341
clovis-p <clovis-p@proton.me>
2026-04-21 13:16:35 -0400

    [45] Remplacer Lambda par référence

--- src/main/java/xyz/lavoute/web/security/SecurityConfig.java
+++ src/main/java/xyz/lavoute/web/security/SecurityConfig.java
@@ -1,9 +1,9 @@
 package xyz.lavoute.web.security;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +11,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/register", "/api/home").permitAll() //Ajouter le /login pour autorisé le endpoint lorsque l'utilisateur n'est pas connecté
                         .anyRequest().authenticated());
