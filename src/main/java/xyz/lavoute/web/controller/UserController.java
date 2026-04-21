dd5bf09457ee66f8d34b1e74e4007551f20b9f3d
clovis-p <clovis-p@proton.me>
2026-04-21 15:04:46 -0400

    [47] Correctif du backend

--- src/main/java/xyz/lavoute/web/controller/UserController.java
+++ src/main/java/xyz/lavoute/web/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
      * @return the HTTPStatus "Created" (201) if the information were valid
      * @throws UserInvalidInformationsException with the error message when some informations are not valid
      */
-    @PostMapping("/api/register")
+    @PostMapping("/register")
     public ResponseEntity<Integer> registerNewUser(@RequestBody RegistrationRequestDTO registrationRequestDto) {
         userService.registerUser(registrationRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
