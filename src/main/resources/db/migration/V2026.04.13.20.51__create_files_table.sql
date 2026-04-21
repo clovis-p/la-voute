2ef45cfac6318f1e6fd33cda703c88dec35ab7e5
wildsource <wildsource@debian>
2026-04-21 14:28:55 -0400

    Make is_directory of files table NOT NULL and false default

--- src/main/resources/db/migration/V2026.04.13.20.51__create_files_table.sql
+++ src/main/resources/db/migration/V2026.04.13.20.51__create_files_table.sql
@@ -1,6 +1,6 @@
 create table files (
     id integer not null auto_increment,
-    is_directory TINYINT(1),
+    is_directory TINYINT(1) NOT NULL DEFAULT 0,
     is_locked TINYINT(1) NOT NULL DEFAULT 0,
     parent_dir_id integer,
     type_id integer,
