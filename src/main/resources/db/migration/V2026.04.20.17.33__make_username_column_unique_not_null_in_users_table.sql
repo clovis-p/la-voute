c13c5cd58ed47d713f811e61f1ed462363170208
wildsource <wildsource@debian>
2026-04-21 13:01:39 -0400

    Remove old migration and move them into username and password field definition

--- src/main/resources/db/migration/V2026.04.20.17.33__make_username_column_unique_not_null_in_users_table.sql
+++
@@ -1,5 +0,0 @@
-UPDATE users SET username = 'user_' || id WHERE username IS NULL;
-
-ALTER TABLE users
-    MODIFY COLUMN username VARCHAR(255) NOT NULL,
-    ADD CONSTRAINT uk_users_username UNIQUE (username);
--- src/main/resources/db/migration/V2026.04.20.18.07__make_password_column_not_null_in_users_table.sql
+++
@@ -1,4 +0,0 @@
-UPDATE users SET password = 'CHANGEME' WHERE password IS NULL;
-
-ALTER TABLE users
-    MODIFY COLUMN password VARCHAR(255) NOT NULL;
