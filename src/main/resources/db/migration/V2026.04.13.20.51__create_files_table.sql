26d268554c243be123bdeb391ef33810e663ba2d
wildsource <wildsource@debian>
2026-04-21 14:02:03 -0400

    Add readme for authentication api

--- src/main/resources/db/migration/V2026.04.13.20.51__create_files_table.sql
+++ src/main/resources/db/migration/V2026.04.13.20.51__create_files_table.sql
@@ -1,7 +1,7 @@
 create table files (
     id integer not null auto_increment,
-    is_directory bit,
-    is_locked bit,
+    is_directory TINYINT(1),
+    is_locked TINYINT(1) NOT NULL DEFAULT 0,
     parent_dir_id integer,
     type_id integer,
     user_id integer not null,
