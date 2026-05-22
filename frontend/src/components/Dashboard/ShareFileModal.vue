a3c6b965375646bb4c6a6053146a235579337db7
clovis-p <clovis-p@proton.me>
2026-05-22 11:12:18 -0400

    [84] (WIP) Modale de partage

--- frontend/src/components/Dashboard/ShareFileModal.vue
+++ frontend/src/components/Dashboard/ShareFileModal.vue
@@ -1,11 +1,25 @@
 <script setup>
-import {Dialog} from "primevue";
+import {Dialog, InputGroup, InputText, InputGroupAddon, Button} from "primevue";
+import {ref} from "vue";
 
 const props = defineProps(['visible', 'file']);
 const emit = defineEmits(['close']);
+
+const shareUrl = "https://lavoute.xyz/file/jk7QJNZW";
+const copied = ref(false);
+
+function copyLink() {
+  navigator.clipboard.writeText(shareUrl);
+  copied.value = true;
+  setTimeout(() => copied.value = false, 2000);
+}
 </script>
 
 <template>
   <Dialog :visible="props.visible" modal :header="`Partager ${props.file?.name}`" :style="{ width: '30rem' }" @update:visible="emit('close')">
+    <InputGroup>
+      <InputText placeholder="Nom d'utilisateur" />
+      <Button icon="pi pi-plus"></Button>
+    </InputGroup>
   </Dialog>
 </template>
